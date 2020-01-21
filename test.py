f3aea05039799969b8e5998151ba0748978ce587
saikedhar <50615626+saikedhar@users.noreply.github.com>
2020-01-21 20:17:03 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = ('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)''')
+cursor.execute(create_table)
+
+
+users =[
+    (1, 'Saikedhar', '5678'),
+  
+]
+
+insert_query = ("INSERT INTO users VALUES (?, ?, ?) ")
+for i in users:
+    cursor.execute(insert_query, (i))
+
+connection.commit()
+
+connection.close()
+print(users)
