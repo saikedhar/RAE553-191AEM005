b69b0f018594ade3c35ee7b16a7db6bfb5b050e5
saikedhar <50615626+saikedhar@users.noreply.github.com>
2020-01-21 18:11:26 +0200

    Add files via upload

---
+++ create_tables.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')#creating data.db database 
+cursor = connection.cursor()# creatting a Cursor object and call its execute() 
+#creating a TABLE for users
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table)
+#creating a TABLE for items
+create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
+cursor.execute(create_table)
+#inserting data for checkitem
+cursor.execute("INSERT INTO items VALUES ('checkitem', '356790')")
+
+connection.commit() # Save 
+connection.close()
